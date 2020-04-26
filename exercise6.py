54a26c0e0315faa01aaab9c41fa94624ef25b672
daniriveradr <43496593+daniriveradr@users.noreply.github.com>
2020-04-25 21:35:49 -0500

    Create exercise6.py
    Write a program that performs the following arithmetic operation (3+2/2⋅5)2.

---
+++ exercise6.py
@@ -0,0 +1,9 @@
+# Escribir un programa que realice la siguiente operación aritmética (3+2/2⋅5)2.
+
+print("operación matematica")
+a = 3
+b = 2
+c = 5
+
+operacion = ((a+b)/(b*c))**b
+print(operacion)
