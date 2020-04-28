3eb81a9962f3c4597d650cca0c02e5d7b7b3be36
daniriveradr <43496593+daniriveradr@users.noreply.github.com>
2020-04-27 21:07:19 -0500

    Create exercise10.py
    Writing a program that asks the user for two integers and displays the <n> between <m> gives a quotient <c> and a remainder <r> where <n> and <m> are the numbers entered by the user, and <c> and <r> are the quotient and the remainder of the integer division respectively.

---
+++ exercise10.py
@@ -0,0 +1,6 @@
+n = float(input("Introduce un número:"))
+m = float(input("Introduce un número:"))
+
+c = n/m
+r = n % m
+print(str(round(n)) + " entre "+ str(round(m)) + " da un cociente "+ str(round(c)) +" y un resto " + str(round(r)))
