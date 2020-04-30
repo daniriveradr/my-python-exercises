c6c3827b9823b14719f47d99066732888c857b15
daniriveradr <43496593+daniriveradr@users.noreply.github.com>
2020-04-30 18:05:54 -0500

    Create exercise6.py

---
+++ conditional/exercise6.py
@@ -0,0 +1,162 @@
+'''Students in a course have been divided into two groups A and B according to gender and name. Group A consists of women with a name before M and men with a name after N and group B for the rest. Write a program that asks the user his/her name and sex, and shows the corresponding group on the screen.
+
+
+Note: Sorry if it looks ugly all the code I didn't know how to use some way to write less lines of code, but the important thing is that it meets the objective and works.
+'''
+nombre = input("Introduce tu nombre ")
+genero = input("Introduce tu genero ")
+nombre = nombre.lower()
+letra = nombre[0]
+# print(letra)
+genero = genero.lower()
+
+if genero == "femenino" and letra == "a":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "n":
+    print("Perteneces al grupo A")
+
+if genero == "femenino" and letra == "b":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "ñ":
+    print("Perteneces al grupo A")    
+
+if genero == "femenino" and letra == "c":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "o":
+    print("Perteneces al grupo A")
+
+if genero == "femenino" and letra == "a":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "p":
+    print("Perteneces al grupo A")
+
+if genero == "femenino" and letra == "b":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "q":
+    print("Perteneces al grupo A")    
+
+if genero == "femenino" and letra == "c":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "r":
+    print("Perteneces al grupo A")
+
+if genero == "femenino" and letra == "d":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "s":
+    print("Perteneces al grupo A")
+
+if genero == "femenino" and letra == "e":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "t":
+    print("Perteneces al grupo A")    
+
+if genero == "femenino" and letra == "f":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "u":
+    print("Perteneces al grupo A")
+
+if genero == "femenino" and letra == "g":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "v":
+    print("Perteneces al grupo A")
+
+if genero == "femenino" and letra == "h":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "w":
+    print("Perteneces al grupo A")    
+
+if genero == "femenino" and letra == "i":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "x":
+    print("Perteneces al grupo A")    
+
+if genero == "femenino" and letra == "j":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "y":
+    print("Perteneces al grupo A")
+
+if genero == "femenino" and letra == "k":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "z":
+    print("Perteneces al grupo A")    
+
+if genero == "femenino" and letra == "l":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "c":
+    print("Perteneces al grupo A")
+
+if genero == "femenino" and letra == "m":
+    print("Perteneces al grupo A")
+elif genero == "masculino" and letra == "a":
+    print("Perteneces al grupo A")
+
+
+
+if genero == "femenino" and letra == "n":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "a":
+    print("Perteneces al grupo B")
+
+if genero == "femenino" and letra == "ñ":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "b":
+    print("Perteneces al grupo B")    
+
+if genero == "femenino" and letra == "p":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "c":
+    print("Perteneces al grupo B")
+
+if genero == "femenino" and letra == "p":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "d":
+    print("Perteneces al grupo B")
+
+if genero == "femenino" and letra == "q":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "e":
+    print("Perteneces al grupo B")    
+
+if genero == "femenino" and letra == "r":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "f":
+    print("Perteneces al grupo B")
+
+if genero == "femenino" and letra == "s":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "g":
+    print("Perteneces al grupo B")
+
+if genero == "femenino" and letra == "t":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "h":
+    print("Perteneces al grupo B")    
+
+if genero == "femenino" and letra == "u":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "i":
+    print("Perteneces al grupo B")
+
+if genero == "femenino" and letra == "v":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "j":
+    print("Perteneces al grupo B")
+
+if genero == "femenino" and letra == "w":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "k":
+    print("Perteneces al grupo B")    
+
+if genero == "femenino" and letra == "x":
+    print("Perteneces al grupo B")
+elif genero == "masculino" and letra == "l":
+    print("Perteneces al grupo B")    
+
+if genero == "femenino" and letra == "y":
+    print("Perteneces al grupo B")
+
+
+if genero == "femenino" and letra == "z":
+    print("Perteneces al grupo B")
+
+
