abb00d703d4b7bfc01403d07b9d51958ce7dc6b5
daniriveradr <43496593+daniriveradr@users.noreply.github.com>
2020-05-01 17:54:58 -0500

    Create exercise1.py

---
+++ looops/exercise1.py
@@ -0,0 +1,8 @@
+'''
+Write a program that asks the user for a word and displays it on the screen 10 times
+'''
+
+print("Escribe una palabra: ")
+palabra= input("")
+for i in range(1, 10):
+    print(palabra)
